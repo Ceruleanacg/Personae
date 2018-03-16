99f9813aa56167b973910279b7cb55c1cc6d05d9
Shuyu <ceruleanacg@gmail.com>
2018-03-16 14:17:35 +0900

    update args parser

--- helper/args_parser.py
+++ helper/args_parser.py
@@ -3,13 +3,13 @@
 import argparse
 
 spider_parser = argparse.ArgumentParser()
-spider_parser.add_argument("-c", "--codes", default=["601288"], type=list)
+spider_parser.add_argument("-c", "--codes", default=["600036, ""601328", "601998", "601288"], type=list)
 spider_parser.add_argument("-s", "--start", default="2008-01-01")
 spider_parser.add_argument("-e", "--end", default="2018-01-01")
 
 
 model_launcher_parser = argparse.ArgumentParser()
 model_launcher_parser.add_argument("-n", "--name", default="DDPG")
-model_launcher_parser.add_argument("-c", "--codes", default=["601328", "601998"], type=list)
+model_launcher_parser.add_argument("-c", "--codes", default=["600036, ""601328", "601998", "601288"], type=list)
 model_launcher_parser.add_argument("-s", "--start_date", default="2008-01-01")
 model_launcher_parser.add_argument("-e", "--end_date", default="2018-01-01")
