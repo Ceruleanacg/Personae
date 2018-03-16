1525783e390d95c886fb384a510af60bfb3a5c19
Shuyu <ceruleanacg@gmail.com>
2018-03-16 17:25:40 +0900

    use pandas

--- base/env/finance.py
+++ base/env/finance.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 
+import pandas as pd
 import numpy as np
 
 import logging
 import math
 
-from spider.finance import StockSpider
 from base.model.finance import Stock
 from sklearn import preprocessing
 from enum import Enum
@@ -56,11 +56,8 @@ class Market(object):
         self.codes = codes
         self.dates = []
 
-        self.stocks_list = []
-
-        self.date_stocks_map = dict()
-        self.date_states_map = dict()
-        self.dates_stocks_pairs = []
+        self.origin_stock_frames = dict()
+        self.scaled_stock_frames = dict()
 
         self.data_dim = None
         self.current_date = None
@@ -81,8 +78,6 @@ class Market(object):
             self.use_state_mix_cash = True
 
         self._init_stocks_data(start_date, end_date)
-        self._init_date_stocks_map()
-        self._init_date_states_map()
 
         self.trader = Trader(self)
 
@@ -93,49 +88,35 @@ class Market(object):
         # Check if codes are valid.
         if not len(self.codes):
             raise ValueError("Initialize, codes cannot be empty.")
+        dates_stocks_pairs = []
         # Init stocks data.
         for code in self.codes:
             # Get stocks data by code.
             stocks = Stock.get_k_data(code, start_date, end_date)
-            self.stocks_list.append(stocks)
-            if not self.data_dim:
-                # Init date dim.
-                self.data_dim = self._get_data_dim(stocks.first())
             # Init stocks dicts.
             stock_dicts = [stock.to_dic() for stock in stocks]
-            # Get dates and stock data.
+            # Get dates and stock data, build frames, save date.
             dates, stocks = [stock[1] for stock in stock_dicts], [stock[2:] for stock in stock_dicts]
-            # Build date map.
+            dates_stocks_pairs.append((dates, stocks))
             [self.dates.append(date) for date in dates if date not in self.dates]
-            # Normalize data.
+            # Cache stock data.
             scaler = preprocessing.MinMaxScaler()
             stocks_scaled = scaler.fit_transform(stocks)
-            # Cache stock data.
-            self.dates_stocks_pairs.append((dates, stocks_scaled))
+            columns = ['open', 'high', 'low', 'close', 'volume']
+            origin_stock_frame = pd.DataFrame(data=stocks, index=dates, columns=columns)
+            scaled_stock_frame = pd.DataFrame(data=stocks_scaled, index=dates, columns=columns)
+            self.origin_stock_frames[code], self.scaled_stock_frames[code] = origin_stock_frame, scaled_stock_frame
+            if not self.data_dim:
+                self.data_dim = self._get_data_dim(origin_stock_frame)
 
-    def _init_date_stocks_map(self):
-        for index, stocks in enumerate(self.stocks_list):
-            for stock in stocks:
-                try:
-                    stock_dic = self.date_stocks_map[stock.date]
-                    stock_dic[stock.code] = stock
-                except KeyError:
-                    self.date_stocks_map[stock.date] = {stock.code: stock}
-
-    def _init_date_states_map(self):
-        for date in self.dates:
-            self.date_states_map[date] = []
-            for pair_index, (dates, stocks) in enumerate(self.dates_stocks_pairs):
-                try:
-                    stock_index = dates.index(date)
-                    stock = stocks[stock_index]
-                    stocks_list = self.date_states_map[date]
-                    stocks_list.append(stock)
-                except ValueError:
-                    self._fill_stop_date_data(date, pair_index)
+        for code in self.codes:
+            origin_stock_frame = self.origin_stock_frames[code].reindex(self.dates, method='bfill')
+            scaled_stock_frame = self.scaled_stock_frames[code].reindex(self.dates, method='bfill')
+            self.origin_stock_frames[code] = origin_stock_frame
+            self.scaled_stock_frames[code] = scaled_stock_frame
 
     def get_cur_stock_data(self, code):
-        return self.date_stocks_map[self.current_date][code]
+        return self.origin_stock_frames[code].loc[self.current_date]
 
     def reset(self):
         self.trader.reset()
@@ -158,7 +139,7 @@ class Market(object):
             action = self.trader.action_dic[action_key]
             try:
                 stock = self.get_cur_stock_data(code)
-                action(stock, 100)
+                action(code, stock, 100)
             except KeyError:
                 logging.info("Current date cannot trade for code: {}.".format(code))
 
@@ -171,40 +152,22 @@ class Market(object):
 
     @property
     def state(self):
-        stocks = np.array([stock for stock in self.date_states_map[self.current_date]])
+        states = np.array([self.scaled_stock_frames[code].loc[self.current_date] for code in self.codes])
         if self.use_one_hot:
-            stocks = stocks.reshape((1, -1))
+            states = states.reshape((1, -1))
             if self.use_state_mix_cash:
-                stocks = np.insert(stocks, 0, self.trader.cash / self.trader.initial_cash, axis=1)
-                stocks = np.insert(stocks, 0, self.trader.holdings_value / self.trader.initial_cash, axis=1)
-        return stocks
+                states = np.insert(states, 0, self.trader.cash / self.trader.initial_cash, axis=1)
+                states = np.insert(states, 0, self.trader.holdings_value / self.trader.initial_cash, axis=1)
+        return states
 
-    def _fill_stop_date_data(self, date, stock_index):
-        index = list(self.date_states_map.keys()).index(date)
-        last_valid_stocks = self._get_last_valid_stocks_data(index)
-        stocks_list = self.date_states_map[date]
-        stocks_list.insert(stock_index, last_valid_stocks[stock_index])
-
-    def _get_data_dim(self, stock):
+    def _get_data_dim(self, data_frame):
         if self.use_one_hot:
-            data_dim = len(self.codes) * len(stock.to_state())
+            data_dim = len(self.codes) * data_frame.shape[1]
             if self.use_state_mix_cash:
                 data_dim += 2
         else:
-            data_dim = len(self.codes) * len(stock.to_state())
+            data_dim = len(self.codes) * data_frame.shape[1]
         return data_dim
-
-    def _get_last_valid_stocks_data(self, index, backward=True):
-        _index = index - 1 if backward else index + 1
-        try:
-            date = list(self.date_states_map.keys())[_index]
-        except IndexError:
-            return self._get_last_valid_stocks_data(_index, backward=False)
-        stocks = self.date_states_map[date]
-        if len(stocks) == len(self.codes):
-            return stocks
-        else:
-            return self._get_last_valid_stocks_data(_index, backward=backward)
 
 
 class Trader(object):
@@ -248,34 +211,34 @@ class Trader(object):
         else:
             return -5
 
-    def buy(self, stock, amount):
+    def buy(self, code, stock, amount):
 
         # Check if amount is OK.
         amount = amount if self.cash > stock.close * amount else int(math.floor(self.cash / stock.close))
 
         if amount == 0:
-            return logging.info("Code: {}, not enough cash.".format(stock.code))
+            return logging.info("Code: {}, not enough cash.".format(code))
 
         # Check if position exists.
-        if not self._exist_position(stock.code):
+        if not self._exist_position(code):
             # Build position if possible.
-            self.positions.append(Position(stock.code, stock.close, amount))
+            self.positions.append(Position(code, stock.close, amount))
         else:
             # Get position and update if possible.
-            position = self._get_position(stock.code)
+            position = self._get_position(code)
             position.add(stock.close, amount)
 
         # Update cash and holding price.
         self.cash -= amount * stock.close
         self._update_profits()
 
-    def sell(self, stock, amount):
+    def sell(self, code, stock, amount):
 
         # Check if position exists.
-        if not self._exist_position(stock.code):
-            return logging.info("Code: {}, not exists in Positions.".format(stock.code))
+        if not self._exist_position(code):
+            return logging.info("Code: {}, not exists in Positions.".format(code))
 
-        position = self._get_position(stock.code)
+        position = self._get_position(code)
 
         # Sell position if possible.
         amount = amount if amount < position.amount else position.amount
@@ -288,7 +251,7 @@ class Trader(object):
         self.cash += amount * stock.close
         self._update_profits()
 
-    def hold(self, stock, amount):
+    def hold(self, code, stock, amount):
         self._update_profits()
 
     def reset(self):
