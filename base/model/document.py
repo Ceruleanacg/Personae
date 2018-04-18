081a8cbcddfdd128b914a113ecb699d0eec7825f
Shuyu <ceruleanacg@gmail.com>
2018-04-18 14:04:20 +0900

    - Add future document

--- base/model/document.py
+++ base/model/document.py
@@ -70,3 +70,51 @@ class Stock(Document):
     @classmethod
     def exist_in_db(cls, code):
         return True if cls.objects(code=code)[:1].count() else False
+
+
+class Future(Document):
+    # 合约代码
+    code = StringField(required=True)
+    # 交易日
+    date = DateTimeField(required=True)
+    # 开盘价
+    open = FloatField()
+    # 最高价
+    high = FloatField()
+    # 最低价
+    low = FloatField()
+    # 收盘价
+    close = FloatField()
+    # 成交量
+    volume = FloatField()
+
+    meta = {
+        'indexes': [
+            'code',
+            'date',
+            ('code', 'date')
+        ]
+    }
+
+    def save_if_need(self):
+        return self.save() if len(self.__class__.objects(code=self.code, date=self.date)) < 1 else None
+
+    def to_state(self):
+        stock_dic = self.to_mongo()
+        stock_dic.pop('_id')
+        stock_dic.pop('code')
+        stock_dic.pop('date')
+        return stock_dic.values()
+
+    def to_dic(self):
+        stock_dic = self.to_mongo()
+        stock_dic.pop('_id')
+        return stock_dic.values()
+
+    @classmethod
+    def get_k_data(cls, code, start, end):
+        return cls.objects(code=code, date__gte=start, date__lte=end).order_by('date')
+
+    @classmethod
+    def exist_in_db(cls, code):
+        return True if cls.objects(code=code)[:1].count() else False
